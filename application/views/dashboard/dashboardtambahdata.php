76bfeb1d0b4f46c2d9235945cb5c2b3992dcf6d6
BintangYustra <bintangyustrawartana@gmail.com>
2025-04-29 13:14:40 +0800

    tabel di dashboard

--- application/views/dashboard/dashboardtambahdata.php
+++ application/views/dashboard/dashboardtambahdata.php
@@ -1,1 +1,10 @@
-<div class="container"><h1>hdfjodhfuh</h1></div>
+<div class="container" style="margin-left: 25px">
+    <h1>Tambah Data Baru</h1>
+    <hr class="sidebar-divider">
+    <form action="">
+        <div class="form-group">
+            <label for="">Nama Subak</label>
+            <input type="text" name="" class="form-control">
+        </div>
+    </form>
+</div>
