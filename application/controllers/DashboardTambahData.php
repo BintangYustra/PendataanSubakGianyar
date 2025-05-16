874e261503ec68a85e5cb56f475bc856cacf9787
BintangYustra <bintangyustrawartana@gmail.com>
2025-05-16 09:55:40 +0800

    nambah nama setiap tabel di controller

--- application/controllers/DashboardTambahData.php
+++ application/controllers/DashboardTambahData.php
@@ -22,6 +22,34 @@ class DashboardTambahData extends CI_Controller {
 	}
 
 	public function AddData(){
+		// nama subak
+		$nama_subak = $this -> input -> post('nama_subak');
+		$kriteria_subak = $this -> input -> post('kriteria_subak');
+		$nomor_akte_notaris = $this -> input -> post('nomor_akte_notaris');
+		$npwp = $this -> input -> post('npwp');
+		$verifikasi = $this -> input -> post('verifikasi');
+
+		// alamat subak
+		$br_lingkungan_subak = $this -> input -> post('br_lingkungan_subak');
+		$desa_subak = $this -> input -> post('desa_subak');
+		$kecamatan_subak = $this -> input -> post('kecamatan_subak');
+		$kabupaten_subak = $this -> input -> post('kabupaten_subak');
+		$kode_pos = $this -> input -> post('kode_pos');
+
+		// prajuru subak
+		$masa_bhakti_ayahan_start = $this -> input -> post('masa_bhakti_ayahan_start');
+		$masa_bhakti_ayahan_end = $this -> input -> post('masa_bhakti_ayahan_end');
+		$pekaseh_nama = $this -> input -> post('pekaseh_nama');
+		$pekaseh_npwp = $this -> input -> post('pekaseh_npwp');
+		$pekaseh_hp_wa = $this -> input -> post('pekaseh_hp_wa');
+		$petajuh_nama = $this -> input -> post('petajuh_nama');
+		$petajuh_npwp = $this -> input -> post('petajuh_npwp');
+		$petajuh_hp_wa = $this -> input -> post('petajuh_hp_wa');
+		$penyarikan_nama = $this -> input -> post('penyarikan_nama');
+		$penyarikan_npwp = $this -> input -> post('penyarikan_npwp');
+		$penyarikan_wa_hp = $this -> input -> post('penyarikan_wa_hp');
+
 		
+		$penyarikan_wa_hp = $this -> input -> post('penyarikan_wa_hp');
 	}
 }
